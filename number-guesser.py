91ca5ce58657d12939a996cf7189abc12d794d81
Vasav Trehan <67497869+Vasav-Trehan@users.noreply.github.com>
2022-04-05 14:27:40 +0530

    Add files via upload

---
+++ number-guesser.py
@@ -0,0 +1,15 @@
+import random
+rand = random.randint(1,100)
+guessed = False
+for chance in range(10):
+    guess = int(input("Try to guess the number: "))
+    if guess == rand:
+        print(f"Congratulations you guessed it correcly in {chance} chances")
+        guessed= True
+        break
+    if guess > rand:
+        print("You guessed a greater value")
+    if guess < rand:
+        print("You guessed a smaller value")
+if guess == False:
+    print(f"You could not guess it the number was {rand}")
